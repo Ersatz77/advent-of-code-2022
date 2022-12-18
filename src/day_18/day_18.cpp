1d034e98f85505165f312a5e9b7fa8c3d4bbf075
Ersatz <cm12mc@gmail.com>
2022-12-18 00:26:43 -0800

    Updated variable names

--- src/day_18/day_18.cpp
+++ src/day_18/day_18.cpp
@@ -22,24 +22,24 @@ namespace aoc
 	{
 		std::ifstream file = open_file(path);
 
-		std::unordered_set<Point_3d> lava_droplets;
+		std::unordered_set<Point_3d> lava_droplet_cubes;
 		int x = 0;
 		int y = 0;
 		int z = 0;
 		char junk = ' ';
 		while (file >> x >> junk >> y >> junk >> z)
 		{
-			lava_droplets.emplace(x, y, z);
+			lava_droplet_cubes.emplace(x, y, z);
 		}
 
-		return lava_droplets;
+		return lava_droplet_cubes;
 	}
 
-	static std::pair<Point_3d, Point_3d> create_bounding_box(const std::unordered_set<Point_3d>& lava_droplets, const int extension = 1)
+	static std::pair<Point_3d, Point_3d> create_bounding_box(const std::unordered_set<Point_3d>& lava_droplet_cubes, const int extension = 1)
 	{
 		Point_3d min{ std::numeric_limits<double>::max(), std::numeric_limits<double>::max() ,std::numeric_limits<double>::max() };
 		Point_3d max{ std::numeric_limits<double>::min(), std::numeric_limits<double>::min() ,std::numeric_limits<double>::min() };
-		for (const Point_3d& droplet : lava_droplets)
+		for (const Point_3d& droplet : lava_droplet_cubes)
 		{
 			min.x = std::min(min.x, droplet.x - extension);
 			min.y = std::min(min.y, droplet.y - extension);
@@ -53,21 +53,21 @@ namespace aoc
 		return std::make_pair(min, max);
 	}
 
-	static bool in_bounding_box(const Point_3d& droplet, const Point_3d& min, const Point_3d& max)
+	static bool in_bounding_box(const Point_3d& cube, const Point_3d& min, const Point_3d& max)
 	{
-		return droplet.x >= min.x && droplet.x <= max.x
-			&& droplet.y >= min.y && droplet.y <= max.y
-			&& droplet.z >= min.z && droplet.z <= max.z;
+		return cube.x >= min.x && cube.x <= max.x
+			&& cube.y >= min.y && cube.y <= max.y
+			&& cube.z >= min.z && cube.z <= max.z;
 	}
 
-	static int64_t surface_area(const std::unordered_set<Point_3d>& lava_droplets, const Point_3d& min, const Point_3d& max)
+	static int64_t surface_area(const std::unordered_set<Point_3d>& lava_droplet_cubes, const Point_3d& min, const Point_3d& max)
 	{
 		int64_t surface_area = 0;
-		for (const Point_3d& droplet : lava_droplets)
+		for (const Point_3d& cube : lava_droplet_cubes)
 		{
-			for (const Point_3d& adj : droplet.adjacent_cardinal())
+			for (const Point_3d& adj : cube.adjacent_cardinal())
 			{
-				if (in_bounding_box(adj, min, max) && !lava_droplets.contains(adj))
+				if (in_bounding_box(adj, min, max) && !lava_droplet_cubes.contains(adj))
 				{
 					++surface_area;
 				}
@@ -77,7 +77,7 @@ namespace aoc
 		return surface_area;
 	}
 
-	static std::unordered_set<Point_3d> flood_fill_box(const std::unordered_set<Point_3d>& lava_droplets, const Point_3d& min, const Point_3d& max)
+	static std::unordered_set<Point_3d> flood_fill_box(const std::unordered_set<Point_3d>& lava_droplet_cubes, const Point_3d& min, const Point_3d& max)
 	{
 		std::unordered_set<Point_3d> visited;
 		std::stack<Point_3d> to_process({ min });
@@ -91,7 +91,7 @@ namespace aoc
 
 			for (const Point_3d& adj : current.adjacent_cardinal())
 			{
-				if (in_bounding_box(adj, min, max) && !lava_droplets.contains(adj))
+				if (in_bounding_box(adj, min, max) && !lava_droplet_cubes.contains(adj))
 				{
 					to_process.push(adj);
 				}
@@ -103,21 +103,21 @@ namespace aoc
 
 	std::string Day_18::part_1(const std::filesystem::path& input_path) const
 	{
-		const std::unordered_set<Point_3d> lava_droplets = parse_input(input_path / "day_18.txt");
-		const auto [min, max] = create_bounding_box(lava_droplets);
+		const std::unordered_set<Point_3d> lava_droplet_cubes = parse_input(input_path / "day_18.txt");
+		const auto [min, max] = create_bounding_box(lava_droplet_cubes);
 
-		int64_t area = surface_area(lava_droplets, min, max);
+		int64_t area = surface_area(lava_droplet_cubes, min, max);
 	
 		return fmt::format("Day 18 Part 1 | Lava droplet surface area: {}", area);
 	}
 
 	std::string Day_18::part_2(const std::filesystem::path& input_path) const
 	{
-		const std::unordered_set<Point_3d> lava_droplets = parse_input(input_path / "day_18.txt");
-		const auto [min, max] = create_bounding_box(lava_droplets);
+		const std::unordered_set<Point_3d> lava_droplet_cubes = parse_input(input_path / "day_18.txt");
+		const auto [min, max] = create_bounding_box(lava_droplet_cubes);
 
-		const std::unordered_set<Point_3d> inverted_lava_droplets = flood_fill_box(lava_droplets, min, max);
-		int64_t area = surface_area(inverted_lava_droplets, min, max);
+		const std::unordered_set<Point_3d> inverted_lava_droplet_cubes = flood_fill_box(lava_droplet_cubes, min, max);
+		int64_t area = surface_area(inverted_lava_droplet_cubes, min, max);
 
 		return fmt::format("Day 18 Part 2 | Real lava droplet surface area: {}", area);
 	}
